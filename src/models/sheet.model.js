935dffad5c1cc3e6f1a096c2edafdbc2424f115f
Atul Yadav <atul99yadav99@gmail.com>
2026-04-11 18:24:19 +0530

    Add sheet model with schema for managing sheets

---
+++ src/models/sheet.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+const sheetschema=new Schema({
+    sheetlink:{
+        type:String,
+        required:true
+    },
+    sheetname:{
+        type:String,
+        required:true
+    },
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    filesize:{
+        type:Number,
+        required:true
+    }, 
+    filepreviewsheets:{
+        type: String , //url cloudinary
+        default:"https://res.cloudinary.com/dzcmadjlq/sheet/upload/v1696543783/ClauseValidator/default_pdf_oyh3v0.png"
+    }
+},{
+    timestamps:true
+})
+sheetschema.plugin(mongooseAggregatePaginate);
+export const Sheet=mongoose.model("Sheet",sheetschema);
